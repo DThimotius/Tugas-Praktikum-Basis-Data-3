7eb38e7441a47c69af50f527b1d0b677e1961a3c
Thimotius <111688744+DThimotius@users.noreply.github.com>
2023-09-16 21:32:00 +0700

    Create saldo_tabungan.sql

---
+++ saldo_tabungan.sql
@@ -0,0 +1,8 @@
+CREATE TABLE saldo_tabungan (
+  no_rekening INT(16) PRIMARY KEY,
+  bukti_slip VARCHAR(10),
+  tanggal_setor DATE,
+  jumlah_setor VARCHAR(10),
+  jenis_rekening VARCHAR(30),
+  kode_transaksi INT(20)
+);
