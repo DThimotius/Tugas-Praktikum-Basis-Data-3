a6acd567e92107576d81bc2c826d60332a34fa4c
Thimotius <111688744+DThimotius@users.noreply.github.com>
2023-09-17 22:53:34 +0700

    direktur_bank.sql

--- direktur_bank.sql
+++ direktur_bank.sql
@@ -1,4 +1,9 @@
 CREATE TABLE direktur_bank (
-  id_direktur INT(20) PRIMARY KEY,
-  nama_direktur VARCHAR(30)
+    id_direktur INT(20) PRIMARY KEY,
+    nama_direktur VARCHAR(30),
+    alamat VARCHAR(50),
+    tanggal_lahir DATE,
+    jenis_kelamin ENUM('L', 'P'),
+    no_telepon VARCHAR(20),
+    email VARCHAR(30)
 );
