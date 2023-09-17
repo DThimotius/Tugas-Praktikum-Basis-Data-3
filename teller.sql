d1240bf229346d9baea5f74a60e7c1825634a4c2
Thimotius <111688744+DThimotius@users.noreply.github.com>
2023-09-17 22:54:02 +0700

    teller.sql

--- teller.sql
+++ teller.sql
@@ -1,4 +1,9 @@
 CREATE TABLE teller (
-  id_teller INT(20) PRIMARY KEY,
-  nama_teller VARCHAR(30)
+    id_teller INT(20) PRIMARY KEY,
+    nama_teller VARCHAR(30),
+    alamat VARCHAR(50),
+    tanggal_lahir DATE,
+    jenis_kelamin ENUM('L', 'P'),
+    no_telepon VARCHAR(20),
+    email VARCHAR(30)
 );
